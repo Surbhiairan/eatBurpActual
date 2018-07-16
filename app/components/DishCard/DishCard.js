f18ad9111f9ff35a93fe93a7361ab02bb61070cd
surbhiairan <surbhiairan@gmail.com>
2018-07-16 16:57:30 +0530

    default image setup

--- app/components/DishCard/DishCard.js
+++ app/components/DishCard/DishCard.js
@@ -7,26 +7,41 @@ import {
   Image, 
   Button } from 'react-native';
 
-const dishCard = (props) => (
+const DEFAULT_IMAGE = require('../../data/images/sandwich.jpg');
+
+const CustomImage = (props) => {
+  if (props.dishImages.length > 0)
+  {
+    return (
+        <Image style={{ width: 200, height: 200 }} source={{ uri: props.dishImages[0] }} />
+      )
+  }
+  else 
+    return <Image source={DEFAULT_IMAGE} style={{ width: 200, height: 200 }} />
+}
+
+const dishCard = (props) => {
+  return (
     <TouchableOpacity onPress={props.onDishCardPressed}>
-    <View style={styles.dishCard}>
-      <Text style={{color: '#000'}}>{props.dish_name}</Text>
-      <Text style={{color: '#000'}}>{props.restaurant_name}</Text>
-      <Text style={{color: '#000'}}>{props.recommended}</Text>
-      <Text style={{color: '#000'}}>{props.average_rating}</Text>
-      <Text style={{color: '#000'}}>{props.reviews[0].review}</Text>
-      <Image source ={{ uri: props.dish_images[0]}} style ={{width: 200, height: 200}}/>
-      <Button 
-        onPress={props.onRecommendButtonPressed}
-        title="Recommend"
-      />
-      <Button 
-        onPress={props.onReviewButtonPressed}
-        title="Review"
-      />
-    </View>
-  </TouchableOpacity>
-);
+      <View style={styles.dishCard}>
+        <Text style={{ color: '#000' }}>{props.dish_name}</Text>
+        <Text style={{ color: '#000' }}>{props.restaurant_name}</Text>
+        <Text style={{ color: '#000' }}>{props.recommended}</Text>
+        <Text style={{ color: '#000' }}>{props.average_rating}</Text>
+        <Text style={{ color: '#000' }}>{props.reviews[0].review}</Text>
+        <CustomImage dishImages = {props.dish_images}/>
+        <Button
+          onPress={props.onRecommendButtonPressed}
+          title="Recommend"
+        />
+        <Button
+          onPress={props.onReviewButtonPressed}
+          title="Review"
+        />
+      </View>
+    </TouchableOpacity>
+  )
+}
 
 const styles = StyleSheet.create({
     dishCard: {
