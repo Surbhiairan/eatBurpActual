455e7eb98b5a5ad78a8486e22e736c66b2171207
Anjali Prajapati <anjaliprajapati3122@gmail.com>
2018-07-20 23:40:19 +0530

    toptenscreen

--- app/components/DishCard/DishCard.js
+++ app/components/DishCard/DishCard.js
@@ -10,26 +10,28 @@ import {
 const DEFAULT_IMAGE = require('../../data/images/sandwich.jpg');
 
 const CustomImage = (props) => {
+  console.log(props, "propsssssssssssssssss");
   if (props.dishImages.length > 0)
   {
     return (
-        <Image style={{ width: 200, height: 200 }} source={{ uri: props.dishImages[0] }} />
+        <Image style={{ width: 316, height: 316, borderRadius: 19 }} source={{ uri: props.dishImages[0] }} />
       )
   }
   else 
-    return <Image source={DEFAULT_IMAGE} style={{ width: 200, height: 200 }} />
+    return <Image source={DEFAULT_IMAGE} style={{ width: 316, height: 316,borderRadius: 19}} />
 }
 
 const dishCard = (props) => {
+  console.log("inside dishcard", props)
   return (
     <TouchableOpacity onPress={props.onDishCardPressed}>
       <View style={styles.dishCard}>
-        <Text style={{ color: '#000' }}>{props.dish_name}</Text>
+        <Text style={{ color: '#212121', fontFamily: 'OpenSans-SemiBold', fontSize: 16 }}>{props.recommended} people recommended this</Text>
+        <CustomImage dishImages = {props.dish_images}/>
+        <Text style={{ color: '#212121', fontFamily: 'OpenSans-Bold', fontSize: 22 }}>{props.dish_name}</Text>
         <Text style={{ color: '#000' }}>{props.restaurant_name}</Text>
-        <Text style={{ color: '#000' }}>{props.recommended}</Text>
         <Text style={{ color: '#000' }}>{props.average_rating}</Text>
         <Text style={{ color: '#000' }}>{props.reviews[0].review}</Text>
-        <CustomImage dishImages = {props.dish_images}/>
         <Button
           onPress={props.onRecommendButtonPressed}
           title="Recommend"
